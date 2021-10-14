a94ca46a50264942a4a8bc59349c909c2ed6eddc
ToniK4 <toni.kneze4@gmail.com>
2021-10-14 19:32:05 +0200

    Add project files.

---
+++ BookRestAPI/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using BookRestAPI.Managers;
+using MandatoryTechClassLibrary;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookRestAPI.Controllers
+{
+    //api/Books
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private BooksManager _manager = new BooksManager();
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _manager.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{isbn}")]
+        public Book Get(string isbn)
+        {
+            return _manager.GetBook(isbn);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BookRestAPI/Managers/BooksManager.cs
@@ -0,0 +1,26 @@
+﻿using MandatoryTechClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookRestAPI.Managers
+{
+    public class BooksManager
+    {
+        private static List<Book> _books = new List<Book>()
+        {
+            new Book{ISBN13 = "9780345418913", Author = "Douglas Adams", PageNumber = 208, Title = "The Hitchhiker's Guide to the Galaxy"},
+            new Book{ISBN13 = "9789531400619", Author = "Sanja Polak", PageNumber = 194, Title = "Dnevnik Pauline P."}
+        };
+        public List<Book> GetAll()
+        {
+            return new List<Book>(_books);
+        }
+
+        public Book GetBook(string isbn)
+        {
+            return _books.Find(book => book.ISBN13.Contains(isbn));
+        }
+    }
+}
